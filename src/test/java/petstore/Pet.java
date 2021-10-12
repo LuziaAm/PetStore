2b7d48d585dc392cbfd8481bf6f4f4ac0df9433e
LuziaAm <Amorim>
2021-10-11 23:45:57 -0400

    Include deletaPet add

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -96,20 +96,20 @@ public class Pet {
     }
 
 
-//    @Test (priority = 3)
-//    public void deletaPet() throws IOException {
-//        String petId = "1982022811102021";
-//
-//        given() // Dado
-//                .contentType("application/json")
-//                .log().all()
-//        .when()
-//                .delete(uri + "/" + petId)
-//
-//        .then()
-//                .log().all()
-//                .statusCode(200)
-//        ;
-//    }
+    @Test (priority = 4)
+    public void deletaPet(){
+        String petId = "1982022811102021";
+
+        given() // Dado
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+    }
 }
     
